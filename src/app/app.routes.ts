13334f074dea0ed11eef8cac2a9865179235db80
MayZry <marirezzouk@gmail.com>
2025-08-17 23:30:28 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AppointmentListComponent } from './components/appointment-list/appointment-list.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { HospitalListComponent } from './components/hospital-list/hospital-list.component';
+import { PatientListComponent } from './components/patient-list/patient-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path:'' , redirectTo:'home' , pathMatch: 'full'},
+    { path: 'home' , component: HomeComponent },
+    { path: 'appointment' , component: AppointmentListComponent},
+    { path: 'dashboard' , component: DashboardComponent},
+    { path: 'hospital' , component: HospitalListComponent},
+    { path: 'patient' , component: PatientListComponent}
+];
